c6253b15859b2022d636584afd7a1bb3e6181d0a
bed-com-07-19 <bed-com-07-19@unima.ac.mw>
2021-12-18 09:17:37 +0200

    the area program has been written

--- Area.java
+++ Area.java
@@ -1,8 +1,25 @@
-
-
-public class Area{
-	public static void main(String[]args){
-		System.out.println("the area will be shown here");
-	}
+import javal.util.Scanner;
+	 
+class Area{
+    private int length;
+    private int width;
+    public Area(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+    public int returnArea(){
+        return length * width;
+    }
 }
 
+public class TryArea{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        Area object = new Area();
+        System.out.println("Enter length ");
+        int length = input.nextInt();
+        System.out.println("Enter width ");
+        int width = input.nextInt();
+
+        System.out.print("The area of the rectangle is " + object.Area(length, width));
+    }
