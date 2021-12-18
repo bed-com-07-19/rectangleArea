dba50436e2aadfc680c4dc80eadd025768878183
bed-com-07-19 <95655887+bed-com-07-19@users.noreply.github.com>
2021-12-18 10:28:13 +0200

    Update Area.java

--- Area.java
+++ Area.java
@@ -1,9 +1,9 @@
 import javal.util.Scanner;
 	 
-class Area{
+class Area1{
     private int length;
     private int width;
-    public Area(int length, int width){
+    public Area1(int length, int width){
         this.length = length;
         this.width = width;
     }
@@ -12,14 +12,15 @@ class Area{
     }
 }
 
-public class TryArea{
+public class Area{
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
-        Area object = new Area();
         System.out.println("Enter length ");
         int length = input.nextInt();
         System.out.println("Enter width ");
         int width = input.nextInt();
+	Area1 object = new Area1(length, width);
 
-        System.out.print("The area of the rectangle is " + object.Area(length, width));
+        System.out.print("The area of the rectangle is " + object.returnArea());
     }
+}
